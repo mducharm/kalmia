b0c98abfa6dd6c392d8991ec0dc1bb1b66a48702
Michael Ducharm <ducharmdev@gmail.com>
2021-04-23 17:30:15 -0400

    added dynamic slider for severity instead

--- src/pages/NewEntry.svelte
+++ src/pages/NewEntry.svelte
@@ -3,10 +3,14 @@
   import Modal from "../components/Modal.svelte";
   import { onMount } from "svelte";
   import type { Label } from "src/types.js";
+  import { getSeverityColor } from "../utils.js";
 
   let showModal = false;
 
   let severity = 1;
+  let severityColor: string;
+  $: severityColor = getSeverityColor(severity);
+
   let medicineTaken = false;
   let notes = "";
   let allLabels: Label[] = [];
@@ -44,37 +48,34 @@
 
 <h1>New Entry</h1>
 
-<form class="flex flex-col mb-5" on:submit|preventDefault>
+<form class="mb-5 bg-white rounded pt-6 pb-8" on:submit|preventDefault>
   <div class="my-3">
     <input type="checkbox" name="medicine-taken" bind:checked={medicineTaken} />
     <label for="medicine-taken">Medicine Taken? </label>
   </div>
 
-  <div class="flex flex-col my-3">
-    <h2>Severity</h2>
-    <label>
-      <input type="radio" bind:group={severity} value={1} />
-      Low
-    </label>
-    <label>
-      <input type="radio" bind:group={severity} value={2} />
-    </label>
-    <label>
-      <input type="radio" bind:group={severity} value={3} />
-      Medium
-    </label>
-    <label>
-      <input type="radio" bind:group={severity} value={4} />
-    </label>
-    <label>
-      <input type="radio" bind:group={severity} value={5} />
-      High
-    </label>
+  <div
+    class="flex flex-col my-3 bg-{severityColor}-200 ring-2 ring-{severityColor}-300 md:mx-auto md:w-1/2"
+  >
+    <h2 class="mx-auto my-3">Severity</h2>
+    <h2 class="mx-auto text-5xl">
+      {severity}
+    </h2>
+    <input
+      class="mx-auto my-3 w-full"
+      type="range"
+      min="1"
+      max="5"
+      bind:value={severity}
+    />
   </div>
 
   <div class="my-3">
-    <input type="date" bind:value={dateOfOccurrence} />
-    <label for="date-of-occurrence">Date of occurrence</label>
+    <input
+      type="date"
+      bind:value={dateOfOccurrence}
+      class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+    />
   </div>
 
   <div class="flex flex-col my-3">
@@ -87,13 +88,20 @@
 
   <div class="my-3">
     <h2>Notes</h2>
-    <textarea bind:value={notes} name="" id="" cols="30" rows="10" />
+    <textarea
+      bind:value={notes}
+      name=""
+      id=""
+      cols="30"
+      rows="10"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
   </div>
 
   <div class="my-3">
     <button
       on:click={saveForm}
-      class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+      class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full"
     >
       Submit
     </button>
