45c480444db2efbb17d42a67b01aa3a19b0fbd51
Michael Ducharm <ducharmdev@gmail.com>
2021-04-24 15:25:37 -0400

    fixed menu spacing issue

--- src/App.svelte
+++ src/App.svelte
@@ -46,9 +46,9 @@
       <ul class="list-reset flex flex-row md:flex-col text-center md:text-left">
         {#each pages as page}
           {#if page.name === $currentPage.name}
-            <li class="mr-3 flex-1">
+            <li class="mr-3 flex-1 justify-self-center">
               <button
-                class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-green-600 focus:outline-none"
+                class="w-full block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-green-600 focus:outline-none"
               >
                 <span
                   class="pb-1 md:pb-0 text-xs md:text-base text-white md:font-bold block md:inline-block"
@@ -61,7 +61,7 @@
             <li class="mr-3 flex-1">
               <button
                 on:click={() => currentPage.set(page)}
-                class="block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-green-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-green-500 focus:outline-none"
+                class="w-full block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-green-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-green-500 focus:outline-none"
               >
                 <span
                   class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
