5277f384f53386f8cd0630c595474b7e88a58eb8
Michael Ducharm <ducharmdev@gmail.com>
2021-04-25 21:28:00 -0400

    use teal for navbar

--- src/App.svelte
+++ src/App.svelte
@@ -43,7 +43,7 @@
 
   <!--Sidebar-->
   <div
-    class="w-full md:w-1/5 bg-gray-900 md:bg-gray-900 px-2 text-center fixed bottom-0 md:pt-8 md:top-0 md:left-0 h-16 md:h-screen md:border-r-4 md:border-gray-600"
+    class="w-full md:w-1/5 bg-teal-900 md:bg-teal-900 px-2 text-center fixed bottom-0 md:pt-8 md:top-0 md:left-0 h-16 md:h-screen md:border-r-4 md:border-gray-600"
   >
     <div class="md:relative mx-auto lg:float-right lg:px-6">
 
@@ -52,7 +52,7 @@
           {#if page.name === $currentPage.name}
             <li class="mr-3 flex-1 justify-self-center">
               <button
-                class="w-full block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-green-600 focus:outline-none"
+                class="w-full block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-teal-600 focus:outline-none"
               >
                 <span
                   class="pb-1 md:pb-0 text-xs md:text-base text-white md:font-bold block md:inline-block"
@@ -65,10 +65,10 @@
             <li class="mr-3 flex-1">
               <button
                 on:click={() => currentPage.set(page)}
-                class="w-full block py-1 md:py-3 pl-1 align-middle text-gray-800 no-underline hover:text-green-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-green-500 focus:outline-none"
+                class="w-full block py-1 md:py-3 pl-1 align-middle text-white-800 no-underline hover:text-teal-500 border-b-2 border-gray-800 md:border-gray-900 hover:border-teal-500 focus:outline-none"
               >
                 <span
-                  class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block"
+                  class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white-400 block md:inline-block"
                 >
                   {page.name}
                 </span>
